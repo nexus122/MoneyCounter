6415044b3fe9b37ef30efcbb540e08664e203460
nexus122 <juan-pablo122@hotmail.es>
2023-12-14 20:30:59 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,73 @@
+class SalarioPorSegundo {
+    salarioBrutoAnual = 26000;
+    amount = 0;
+    time = 0;
+    resultado = document.getElementById("resultado");
+    contador = document.getElementById("contador");
+    intervalId = null;
+    playButton = document.getElementById("playButton");
+    pauseButton = document.getElementById("pauseButton");
+    resetButton = document.getElementById("resetButton");
+
+    constructor() {
+        this.playButton.addEventListener("click", () => this.start());
+        this.pauseButton.addEventListener("click", () => this.pause());
+        this.resetButton.addEventListener("click", () => this.reset());
+    }
+
+    start = () => {
+        if (this.intervalId === null) {
+            this.intervalId = setInterval(() => {
+                this.addAmount();
+                this.updateTime();
+            }, 1000);
+        }
+    };
+
+    pause = () => {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    };
+
+    reset = () => {
+        this.amount = 0;
+        this.time = 0;
+        this.drawAmount();
+        this.resetTime();
+    };
+
+    getSalarioBrutoSegundo = () => {
+        return this.salarioBrutoAnual / 12 / 22 / 8 / 60 / 60;
+    };
+
+    addAmount = () => {
+        this.amount += this.getSalarioBrutoSegundo();
+        this.drawAmount();
+    };
+
+    drawAmount = () => {
+        this.resultado.innerHTML = `${this.amount.toFixed(2)}€`;
+        document.title = `${this.amount.toFixed(2)}€ ${this.getTimeString()}`;
+    };
+
+    updateTime = () => {
+        this.time++;
+        this.contador.innerHTML = this.getTimeString();
+        document.title = `${this.amount.toFixed(2)}€ ${this.getTimeString()}`;
+    };
+
+    resetTime = () => {
+        this.time = 0;
+        this.contador.innerHTML = "00:00:00";
+        document.title = "0.00€ 00:00:00";
+    };
+
+    getTimeString = () => {
+        const hours = Math.floor(this.time / 3600);
+        const minutes = Math.floor((this.time % 3600) / 60);
+        const seconds = this.time % 60;
+        return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+    };
+}
+
+const main = new SalarioPorSegundo();
